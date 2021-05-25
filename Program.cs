622d950ac578de0e8808b6aa7a93783247d8d5c8
Brandon Hill <brandonjon26@gmail.com>
2021-05-25 15:04:25 -0500

    phase 5 is complete

--- Program.cs
+++ Program.cs
@@ -4,36 +4,35 @@ Console.WriteLine("Lets play a game!");
 Console.WriteLine("Please guess the secret number!");
 Console.WriteLine("");
 
-int attempts = 1;
+int attempts = 6;
 Main();
 
 void Main()
 {
-    string secretNumber = "42";
+    int secretNumber = new Random().Next(1, 100);
 
     Console.Write("Your guess> ");
     string number = Console.ReadLine();
+    int guess = int.Parse(number);
 
     if (string.IsNullOrWhiteSpace(number))
     {
         Console.WriteLine("Your loss, this game is fun!");
         return;
     }
-    else if (number == secretNumber && attempts < 4)
+    else if (guess == secretNumber && attempts > 0)
     {
         Console.WriteLine($"Your guess of {number} is CORRECT!");
     }
-    else if (number != secretNumber && attempts == 4)
+    else if (guess != secretNumber && attempts == 0)
     {
-        Console.WriteLine($"WRONG!!! Atempt number {attempts}, no more guesses.");
+        Console.WriteLine($"WRONG!!! Guesses Remaining {attempts}, no more guesses.");
     }
     else
     {
-        Console.WriteLine($"{number} is incorrect... YOU SUCK! (attempt number {attempts})");
-        attempts++;
+        Console.WriteLine($"{number} is incorrect... YOU SUCK! (Guesses remaining {attempts})");
+        attempts--;
         Main();
 
     }
 }
-
-// I already completed phase 4 in phase 3... oops
