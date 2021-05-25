11cec2d3e4e1ba62ddffc6e925a6380791f6155d
Brandon Hill <brandonjon26@gmail.com>
2021-05-25 14:03:49 -0500

    phase 3 is complete

--- Program.cs
+++ Program.cs
@@ -4,32 +4,34 @@ Console.WriteLine("Lets play a game!");
 Console.WriteLine("Please guess the secret number!");
 Console.WriteLine("");
 
-Console.Write("Your guess> ");
-string number = Console.ReadLine();
+int attempts = 1;
+Main();
 
-string secretNumber = "42";
-
-if (string.IsNullOrWhiteSpace(number))
+void Main()
 {
-    Console.WriteLine("Your loss, this game is fun!");
-    return;
-}
-else if (number == secretNumber)
-{
-    Console.WriteLine($"Your guess of {number} is CORRECT!");
-}
-else
-{
-    Console.WriteLine($"{number} is incorrect... YOU SUCK!");
-}
+    string secretNumber = "42";
 
+    Console.Write("Your guess> ");
+    string number = Console.ReadLine();
 
+    if (string.IsNullOrWhiteSpace(number))
+    {
+        Console.WriteLine("Your loss, this game is fun!");
+        return;
+    }
+    else if (number == secretNumber && attempts < 4)
+    {
+        Console.WriteLine($"Your guess of {number} is CORRECT!");
+    }
+    else if (number != secretNumber && attempts == 4)
+    {
+        Console.WriteLine($"WRONG!!! Atempt number {attempts}, no more guesses.");
+    }
+    else
+    {
+        Console.WriteLine($"{number} is incorrect... YOU SUCK! (attempt number {attempts})");
+        attempts++;
+        Main();
 
-// if (number == secretNumber)
-// {
-//     Console.WriteLine($"Your guess of {number} is CORRECT!");
-// }
-// else
-// {
-//     Console.WriteLine($"{number} is incorrect... YOU SUCK!");
-// }
+    }
+}
