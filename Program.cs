36094cdfbc3264ccf6ba6c6bb56d4316285fee20
Brandon Hill <brandonjon26@gmail.com>
2021-05-25 11:30:17 -0500

    phase 1 is complete

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using System;
 
+Console.WriteLine("Lets play a game!");
+Console.WriteLine("Please guess the secret number!");
+Console.WriteLine("");
 
+Console.Write("Your guess> ");
+string number = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(number))
+{
+    Console.WriteLine("Your loss, this game is fun!");
+}
+else
+{
+    Console.WriteLine($"You guessed {number}");
+}
